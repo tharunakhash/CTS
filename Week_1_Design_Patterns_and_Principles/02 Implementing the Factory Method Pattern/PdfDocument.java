3dd17993f3529c8b6afed961388071e02d47f113
tharunakhash <129102767+tharunakhash@users.noreply.github.com>
2025-06-29 23:09:01 +0530

    Add files via upload

---
+++ Week_1_Design_Patterns_and_Principles/02 Implementing the Factory Method Pattern/PdfDocument.java
@@ -0,0 +1,11 @@
+public class PdfDocument implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening PDF document.");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Closing PDF document.");
+    }
+}
