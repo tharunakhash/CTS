a39579c54ec113da5d36d3f0f3a25931b645157f
tharunakhash <129102767+tharunakhash@users.noreply.github.com>
2025-06-29 23:06:35 +0530

    Add files via upload

---
+++ Week_1_Design_Patterns_and_Principles/01 Implementing the Singleton Pattern/Logger.java
@@ -0,0 +1,23 @@
+public class Logger {
+    private static volatile Logger instance;
+
+    private Logger() {
+        // Initialization code
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            synchronized (Logger.class) {
+                if (instance == null) {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+
